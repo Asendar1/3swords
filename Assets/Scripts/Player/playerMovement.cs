1b562a2f7dc9ee7a2fd25c1ebff9b5c690ad28ac
Asendar1 <hamzahtaimour@gmail.com>
2025-07-13 15:13:23 +0300

    move speed is good bruv

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -1,16 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private float moveSpeed = 9f;
+
+    private Vector2 moveInput;
+
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private CharacterController controller;
+
+    private void OnEnable()
     {
-        
+        playerInput.actions["Move"].performed += OnMove;
+        playerInput.actions["Move"].canceled += OnMove;
     }
 
-    // Update is called once per frame
-    void Update()
+	private void OnDisable()
+	{
+        playerInput.actions["Move"].performed -= OnMove;
+        playerInput.actions["Move"].canceled -= OnMove;
+	}
+
+	void Update()
     {
-        
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+	}
+
+	private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
